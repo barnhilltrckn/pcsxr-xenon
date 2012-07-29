ea4b15f14984773e5966b9ffa5db32251e526ef2
barnhilltrckn <barnhilltrckn@gmail.com>
2012-07-29 15:07:53 -0300

    ???

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -43,106 +43,106 @@ extern "C" void httpd_start(void);
 
 extern PluginTable plugins[];
 
-// #define cdfile "uda:/psxisos/BREATH_OF_FIRE3.BIN"
-#define cdfile "uda:/psxisos/MEGAMAN8.BIN"
-// #define cdfile "uda:/psxisos/RPG Maker (U).bin"
-#define cdfile "uda:/psxisos/Vagrant Story[NTSC-U].iso"
-// #define cdfile "uda:/psx.iso"
-// #define cdfile "uda:/psxisos/Super Street Fighter Collection.bin"
-#define cdfile "uda:/psxisos/Strider 2 (USA)/Strider 2 (USA).bin"
-#define cdfile "uda:/psxisos/Street Fighter Alpha - Warriors' Dreams (USA)/Street Fighter Alpha - Warriors' Dreams (USA) (Track 01).bin"
-// #define cdfile "uda:/psxisos/Street Fighter Alpha 2 (USA)/Street Fighter Alpha 2 (USA) (Track 1).bin"
-#define cdfile "uda:/psxisos/odd.iso"
-#define cdfile "uda:/psxisos/MEGAMAN8.BIN"
-#define cdfile "uda:/psxisos/PSX_Tekken_3_(NTSC).Iso"
+// #define cdfile "uda0:/psxisos/BREATH_OF_FIRE3.BIN"
+#define cdfile "uda0:/psxisos/MEGAMAN8.BIN"
+// #define cdfile "uda0:/psxisos/RPG Maker (U).bin"
+#define cdfile "uda0:/psxisos/Vagrant Story[NTSC-U].iso"
+// #define cdfile "uda0:/psx.iso"
+// #define cdfile "uda0:/psxisos/Super Street Fighter Collection.bin"
+#define cdfile "uda0:/psxisos/Strider 2 (USA)/Strider 2 (USA).bin"
+#define cdfile "uda0:/psxisos/Street Fighter Alpha - Warriors' Dreams (USA)/Street Fighter Alpha - Warriors' Dreams (USA) (Track 01).bin"
+// #define cdfile "uda0:/psxisos/Street Fighter Alpha 2 (USA)/Street Fighter Alpha 2 (USA) (Track 1).bin"
+#define cdfile "uda0:/psxisos/odd.iso"
+#define cdfile "uda0:/psxisos/MEGAMAN8.BIN"
+#define cdfile "uda0:/psxisos/PSX_Tekken_3_(NTSC).Iso"
 
-//#define cdfile "uda:/psxisos/Street Fighter Alpha - Warriors' Dreams (USA)/Street Fighter Alpha - Warriors' Dreams (USA) (Track 01).bin"
-//#define cdfile "uda:/psxisos/Soul Blade (USA) (v1.1).bin"
+//#define cdfile "uda0:/psxisos/Street Fighter Alpha - Warriors' Dreams (USA)/Street Fighter Alpha - Warriors' Dreams (USA) (Track 01).bin"
+//#define cdfile "uda0:/psxisos/Soul Blade (USA) (v1.1).bin"
 
 
-//#define cdfile "uda:/psxisos/Strider 2 (USA)/Strider 2 (USA).bin"
-//#define cdfile "uda:/psxisos/CTR - Crash Team Racing (USA).bin"
-//#define cdfile "uda:/psxisos/Strider 2 (USA)/Strider 2 (USA).bin"
-//#define cdfile "uda:/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
+//#define cdfile "uda0:/psxisos/Strider 2 (USA)/Strider 2 (USA).bin"
+//#define cdfile "uda0:/psxisos/CTR - Crash Team Racing (USA).bin"
+//#define cdfile "uda0:/psxisos/Strider 2 (USA)/Strider 2 (USA).bin"
+//#define cdfile "uda0:/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
 
 /*
-#define cdfile "uda:/psxisos/Strider 2 (USA)/Strider 2 (USA).bin"
+#define cdfile "uda0:/psxisos/Strider 2 (USA)/Strider 2 (USA).bin"
 
-#define cdfile "uda:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
+#define cdfile "uda0:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
 
-#define cdfile "uda:/psxisos/Soul Blade (USA) (v1.1).bin"
-#define cdfile "uda:/psxisos/Final Fantasy VII (USA)/Final Fantasy VII (USA) (Disc 1).bin"
+#define cdfile "uda0:/psxisos/Soul Blade (USA) (v1.1).bin"
+#define cdfile "uda0:/psxisos/Final Fantasy VII (USA)/Final Fantasy VII (USA) (Disc 1).bin"
 
 
-#define cdfile "uda:/psxisos/Crash Bandicoot - Warped (USA).bin"
-#define cdfile  "uda:/psxisos/Final Fantasy VII (F)/Final Fantasy VII (F) (Disc 1) [SCES-00868].bin"
+#define cdfile "uda0:/psxisos/Crash Bandicoot - Warped (USA).bin"
+#define cdfile  "uda0:/psxisos/Final Fantasy VII (F)/Final Fantasy VII (F) (Disc 1) [SCES-00868].bin"
 
-#define cdfile "uda:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
+#define cdfile "uda0:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
  */
 /*
-#define cdfile "uda:/psxisos/CTR - Crash Team Racing (USA).bin"
+#define cdfile "uda0:/psxisos/CTR - Crash Team Racing (USA).bin"
  */
-//#define cdfile "uda:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
-//#define cdfile "uda:/psxisos/CTR - Crash Team Racing (USA).bin"
-#define cdfile "uda:/psxisos/Final Fantasy VII (USA)/Final Fantasy VII (USA) (Disc 1).bin"
-//#define cdfile "uda:/psxisos/Resident Evil - Director's Cut - Dual Shock Ver. (USA)/Resident Evil - Director's Cut - Dual Shock Ver. (USA) (Track 1).bin"
-//#define cdfile "uda:/psxiso.iso"
+//#define cdfile "uda0:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
+//#define cdfile "uda0:/psxisos/CTR - Crash Team Racing (USA).bin"
+#define cdfile "uda0:/psxisos/Final Fantasy VII (USA)/Final Fantasy VII (USA) (Disc 1).bin"
+//#define cdfile "uda0:/psxisos/Resident Evil - Director's Cut - Dual Shock Ver. (USA)/Resident Evil - Director's Cut - Dual Shock Ver. (USA) (Track 1).bin"
+//#define cdfile "uda0:/psxiso.iso"
 //#define cdfile "uda:/psxisos/Gran Turismo 2 (USA) (v1.0) (Simulation Mode)/Gran Turismo 2 (USA) (v1.0) (Simulation Mode).bin"
 
-#define cdfile "uda:/psxisos/Street Fighter Alpha 2 (USA)/Street Fighter Alpha 2 (USA) (Track 1).bin"
-//#define cdfile "uda:/psxiso.iso"
-//#define cdfile "uda:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
-//#define cdfile  "uda:/psxisos/Final Fantasy VII (F)/Final Fantasy VII (F) (Disc 1) [SCES-00868].bin"
-//#define cdfile "uda:/psxisos/CTR - Crash Team Racing (USA).bin"
-//#define cdfile "uda:/psxisos/Crash Bandicoot - Warped (USA).bin"
-//#define cdfile "uda:/psxisos/WipEout XL (USA)/WipEout XL (USA) (Track 01).bin"
-//#define cdfile "uda:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
-//#define cdfile "uda:/psxiso.iso"
-//#define cdfile "uda:/psxisos/Rayman (USA)/Rayman (USA) (Track 01).bin"
-//#define cdfile "uda:/psxisos/Darkstalkers 3 (USA)/Darkstalkers 3 (USA).bin"
-//#define cdfile "uda:/psxisos/Gran Turismo 2 (USA) (v1.0) (Simulation Mode)/Gran Turismo 2 (USA) (v1.0) (Simulation Mode).bin"
+#define cdfile "uda0:/psxisos/Street Fighter Alpha 2 (USA)/Street Fighter Alpha 2 (USA) (Track 1).bin"
+//#define cdfile "uda0:/psxiso.iso"
+//#define cdfile "uda0:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
+//#define cdfile  "uda0:/psxisos/Final Fantasy VII (F)/Final Fantasy VII (F) (Disc 1) [SCES-00868].bin"
+//#define cdfile "uda0:/psxisos/CTR - Crash Team Racing (USA).bin"
+//#define cdfile "uda0:/psxisos/Crash Bandicoot - Warped (USA).bin"
+//#define cdfile "uda0:/psxisos/WipEout XL (USA)/WipEout XL (USA) (Track 01).bin"
+//#define cdfile "uda0:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
+//#define cdfile "uda0:/psxiso.iso"
+//#define cdfile "uda0:/psxisos/Rayman (USA)/Rayman (USA) (Track 01).bin"
+//#define cdfile "uda0:/psxisos/Darkstalkers 3 (USA)/Darkstalkers 3 (USA).bin"
+//#define cdfile "uda0:/psxisos/Gran Turismo 2 (USA) (v1.0) (Simulation Mode)/Gran Turismo 2 (USA) (v1.0) (Simulation Mode).bin"
 
-//#define cdfile "uda:/psxisos/Street Fighter Alpha - Warriors' Dreams (USA)/Street Fighter Alpha - Warriors' Dreams (USA) (Track 01).bin"
-//#define cdfile "uda:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
+//#define cdfile "uda0:/psxisos/Street Fighter Alpha - Warriors' Dreams (USA)/Street Fighter Alpha - Warriors' Dreams (USA) (Track 01).bin"
+//#define cdfile "uda0:/psxisos/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin"
 
 
-//#define cdfile "uda:/psxisos/CTR - Crash Team Racing (USA).bin"
-//#define cdfile "uda:/psxisos/R4 - Ridge Racer Type 4 (USA)/R4 - Ridge Racer Type 4 (USA).bin"
+//#define cdfile "uda0:/psxisos/CTR - Crash Team Racing (USA).bin"
+//#define cdfile "uda0:/psxisos/R4 - Ridge Racer Type 4 (USA)/R4 - Ridge Racer Type 4 (USA).bin"
 
-// #define cdfile "uda:/psxisos/Metal Gear Solid (France) (Disc 1)/Metal Gear Solid (France) (Disc 1).bin"
-//#define cdfile "uda:/psxisos/Gran Turismo 2 (USA) (v1.0) (Simulation Mode)/Gran Turismo 2 (USA) (v1.0) (Simulation Mode).bin"
+// #define cdfile "uda0:/psxisos/Metal Gear Solid (France) (Disc 1)/Metal Gear Solid (France) (Disc 1).bin"
+//#define cdfile "uda0:/psxisos/Gran Turismo 2 (USA) (v1.0) (Simulation Mode)/Gran Turismo 2 (USA) (v1.0) (Simulation Mode).bin"
 //#define cdfile "sda:/hdd1/xenon/psx/CTR - Crash Team Racing (USA)/CTR - Crash Team Racing (USA).bin.Z"
 //#define cdfile "sda:/hdd1/xenon/psx/Tekken 3 (USA)/Tekken 3 (USA) (Track 1).bin.Z"
 //#define cdfile "sda:/hdd1/xenon/psx/R4 - Ridge Racer Type 4 (USA).bin"
-#define cdfile "uda:/CTR - Crash Team Racing (USA).bin"
-//#define cdfile "uda:/Street Fighter Alpha - Warriors' Dreams (USA) (Track 01).bin"
-//#define cdfile "uda:/Tekken 3 (USA) (Track 1).bin.Z"
+#define cdfile "uda0:/CTR - Crash Team Racing (USA).bin"
+//#define cdfile "uda0:/Street Fighter Alpha - Warriors' Dreams (USA) (Track 01).bin"
+//#define cdfile "uda0:/Tekken 3 (USA) (Track 1).bin.Z"
 //
-//#define cdfile "uda:/R4 - Ridge Racer Type 4 (USA).bin.Z"
-//#define cdfile "uda:/Final Fantasy VII (USA) (Disc 1).bin"
-#define cdfile "uda:/Tekken 3 (USA) (Track 1).bin.Z"
+//#define cdfile "uda0:/R4 - Ridge Racer Type 4 (USA).bin.Z"
+//#define cdfile "uda0:/Final Fantasy VII (USA) (Disc 1).bin"
+#define cdfile "uda0:/Tekken 3 (USA) (Track 1).bin.Z"
 
-//#define cdfile "uda:/CTR - Crash Team Racing (USA).bin.Z"
+//#define cdfile "uda0:/CTR - Crash Team Racing (USA).bin.Z"
 
-#define cdfile "uda:/Resident Evil (USA).iso.Z"
-#define cdfile "uda:/0cimg/medievil-scus-94227-/Medievil.bin.Z"
+#define cdfile "uda0:/Resident Evil (USA).iso.Z"
+#define cdfile "uda0:/0cimg/medievil-scus-94227-/Medievil.bin.Z"
 
 
-#define cdfile "uda:/Street Fighter Alpha 2 [SLUS-00258].img.Z"
-#define cdfile "uda:/ff9_patched.bin"
-#define cdfile "usb:/Final Fantasy IX (France) (Disc 1).bin.Z"
+#define cdfile "uda0:/Street Fighter Alpha 2 [SLUS-00258].img.Z"
+#define cdfile "uda0:/ff9_patched.bin"
+#define cdfile "usb0:/Final Fantasy IX (France) (Disc 1).bin.Z"
 
-#define cdfile "uda:/pcsxr/iso/Final Fantasy IX (France) (Disc 1).bin"
-//#define cdfile "uda:/pcsxr/iso/Crash Bandicoot 3.bin"
+#define cdfile "uda0:/pcsxr/iso/Final Fantasy IX (France) (Disc 1).bin"
+//#define cdfile "uda0:/pcsxr/iso/Crash Bandicoot 3.bin"
 
-//#define cdfile "uda:/pcsxr/iso/medievil2.img.Z"
+//#define cdfile "uda0:/pcsxr/iso/medievil2.img.Z"
 
 #define cdfile "sda0:/devkit/pcsxr/tekken3.bin"
 
 
-//#define cdfile "uda:/pcsxr/iso/sfa.bin"
+//#define cdfile "uda0:/pcsxr/iso/sfa.bin"
 
-//#define cdfile "uda:/pcsxr/iso/toshiden2.img"
+//#define cdfile "uda0:/pcsxr/iso/toshiden2.img"
 
 void printConfigInfo() {
 
